f97fd515fd7f04ae3d4f13e68a9bd935f0666173
ElisaME <elisam.esquivel@gmail.com>
2016-06-10 14:03:42 -0500

    Borrador de función agregar mensaje

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
-//Imprime en consola el valor del input al hacer enter
+
+// Imprime en consola el valor del input al hacer enter
 $('#nuevoMensaje').keydown(function(e){
 	var key = e.which;
 	if(key == 13) {
 		var mensaje=$(this).val();
-		console.log(mensaje)
+		console.log(mensaje);
 	}
 });
 
-// var mensaje= $("#nuevoMensaje").val()
+// function agregarMensaje(){
+// 	var mensaje=document.createElement('p');
+// 	var contenidoMensaje=document.createTextNode('hola')
+// 	mensaje.appendChild(contenidoMensaje)
+// 	document.getElementById('chat').appendChild('mensaje')
+// }
+// var valorMensaje=document.getElementById('nuevoMensaje');
+// valorMensaje.onkeyup=function(event){
+// 	if (event.which == 13){
+// 		agregarMensaje;
+// 	}
+// }
+
