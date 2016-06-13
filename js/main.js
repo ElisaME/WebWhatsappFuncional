d49f7d2bad051b51fd2ab0c4192817ca1cb7dccb
ElisaME <elisam.esquivel@gmail.com>
2016-06-13 09:25:03 -0500

    Se agrega mensaje

--- js/main.js
+++ js/main.js
@@ -1,23 +1,38 @@
 
-// Imprime en consola el valor del input al hacer enter
-$('#nuevoMensaje').keydown(function(e){
-	var key = e.which;
-	if(key == 13) {
-		var mensaje=$(this).val();
-		console.log(mensaje);
-	}
+//FUNCIONA
+var cajaChat = document.getElementById("burbujaChat"),
+	mensajeInput = document.getElementById("mensajeInput"),
+	btnNuevaTarea = document.getElementById("btn-agregar");
+
+// Funciones
+var agregarTarea = function(){
+	var mensaje = mensajeInput.value,
+		nuevoMensaje = document.createElement("div"),
+		contenido = document.createTextNode(mensaje);
+
+	nuevoMensaje.setAttribute("class", "text-box, right");
+	// Agregamos el contenido al div
+	nuevoMensaje.appendChild(contenido);
+	// Agregamos nuevo mensaje a chat
+	cajaChat.appendChild(nuevoMensaje);
+	mensajeInput.value = "";
+};
+
+$(document).ready(function(){
+	// Agregar Mensaje con boton
+	btnNuevaTarea.addEventListener("click", agregarTarea);
 });
 
-// function agregarMensaje(){
-// 	var mensaje=document.createElement('p');
-// 	var contenidoMensaje=document.createTextNode('hola')
-// 	mensaje.appendChild(contenidoMensaje)
-// 	document.getElementById('chat').appendChild('mensaje')
-// }
-// var valorMensaje=document.getElementById('nuevoMensaje');
-// valorMensaje.onkeyup=function(event){
-// 	if (event.which == 13){
-// 		agregarMensaje;
-// 	}
-// }
 
+mensajeInput.onkeyup = function(e){
+	if (e.which == 13){
+		agregarTarea;
+	}
+}
+//Imprime en consola el valor del input al hacer enter
+// $('#nuevoMensaje').keydown(function(e){
+// 	var key = e.which;
+// 	if(key == 13) {
+// 		addEventListener(agregarTarea);
+// 	}
+// });
