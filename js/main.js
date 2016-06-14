eb1469582f4eab10cd4eeb4713ce7e688eb58ae9
ElisaME <elisam.esquivel@gmail.com>
2016-06-14 10:47:29 -0500

    Funciona búsqueda y cambia nombre de usuario según la conversación que eligas, se envía contenido de input

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@ var fotos = ['logocodeacademy.png', 'raymi.jpg',
 
 var mensajes=[];
 // Funciones
+//Agregar Tarea
 var agregarTarea = function(){
 	var mensaje = mensajeInput.value,
 		nuevoMensaje = document.createElement("div"),
@@ -26,10 +27,51 @@ var agregarTarea = function(){
 	// Agregamos nuevo mensaje a chat
 	cajaChat.appendChild(nuevoMensaje);
 	mensajeInput.value = "";
+
 };
+// Agregar Mensaje
+var agregarMensaje = function(){
+	var mensaje = mensajeInput.value;
+	var contacto= $('#usuario').text();
+	var temp = {
+		mensaje: mensaje,
+		persona:contacto
+	};
+	mensajes.push(temp);
+	//no se escribe el mensaje, No se ve en el array
+	console.log(mensaje)
+}
 
+//Buscar mensaje
+function doSearch(){
+	var tableReg = document.getElementById('conversaciones');
+	var searchText = document.getElementById('searchTerm').value.toLowerCase();
+	var cellsOfRow="";
+	var found=false;
+	var compareWith="";
 
-//
+	// Recorremos todas las filas con contenido de la tabla
+	for (var i = 1; i < tableReg.rows.length; i++) {
+		cellsOfRow = tableReg.rows[i].getElementsByTagName('p');
+		found = false;
+		// Recorremos todas las celdas
+		for (var j = 0; j < cellsOfRow.length && !found; j++){
+			compareWith = cellsOfRow[j].innerHTML.toLowerCase();
+			// Buscamos el texto en el contenido de la celda
+			if (searchText.length == 0 || (compareWith.indexOf(searchText) > -1)){
+				found = true;
+			}
+		}
+		if(found){
+			tableReg.rows[i].style.display = '';
+		} else {
+			// si no ha encontrado ninguna coincidencia, esconde la
+			// fila de la tabla
+			tableReg.rows[i].style.display = 'none';
+		}
+	}
+}
+
 $(document).ready(function(){
 	// Agregar Mensaje con enter
 	$('#mensajeInput').keypress(function(e){
@@ -40,15 +82,8 @@ $(document).ready(function(){
 				return false
 			}
 			agregarTarea();
+			agregarMensaje();
 		}
-		//Agregar mensaje a arreglo
-		// var texto = $('#mensajeInput').val();
-		// var contacto= $('#usuario').text();
-		// var temp = {
-		// 	mensaje: texto,
-		// 	persona:contacto
-		// };
-		// mensajes.push('hola');
 	});
 	//Cambiar nombre de conversación
 	$('.conversacion').click(function(){
